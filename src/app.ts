d2bb36e5ca01effd494189191ef7d1c240d8c573
the-nazzmul <nazmul.hussain.utchash@gmail.com>
2023-11-23 23:18:43 +0600

    project setup

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,14 @@
-const express = require("express");
-const app = express();
-const port = 4000;
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const app: Application = express();
+
+// parsers
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World!');
 });
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,16 @@
-import app from "./app";
-import mongoose from "mongoose";
+import app from './app';
+import mongoose from 'mongoose';
+import config from './app/config';
 
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/test");
+  try {
+    await mongoose.connect(config.database_url as string);
+    app.listen(config.port, () => {
+      console.log(`App listening on port ${config.port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+main();
